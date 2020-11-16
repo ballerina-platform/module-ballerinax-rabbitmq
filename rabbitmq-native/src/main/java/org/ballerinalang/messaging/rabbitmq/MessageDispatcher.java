c41b79a266821358d695a54825cd77719e8dcb94
aashikam <arshika.nm@gmail.com>
2020-11-16 10:46:51 +0530

    Migrate ballerina runtime APIs

--- rabbitmq-native/src/main/java/org/ballerinalang/messaging/rabbitmq/MessageDispatcher.java
+++ rabbitmq-native/src/main/java/org/ballerinalang/messaging/rabbitmq/MessageDispatcher.java
@@ -23,29 +23,25 @@ import com.rabbitmq.client.AlreadyClosedException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-import io.ballerina.runtime.JSONParser;
-import io.ballerina.runtime.JSONUtils;
-import io.ballerina.runtime.XMLFactory;
 import io.ballerina.runtime.api.Runtime;
-import io.ballerina.runtime.api.StringUtils;
 import io.ballerina.runtime.api.TypeTags;
-import io.ballerina.runtime.api.ValueCreator;
 import io.ballerina.runtime.api.async.Callback;
 import io.ballerina.runtime.api.async.StrandMetadata;
+import io.ballerina.runtime.api.creators.ValueCreator;
+import io.ballerina.runtime.api.types.AnnotatableType;
 import io.ballerina.runtime.api.types.AttachedFunctionType;
+import io.ballerina.runtime.api.types.StructureType;
 import io.ballerina.runtime.api.types.Type;
+import io.ballerina.runtime.api.utils.JsonUtils;
+import io.ballerina.runtime.api.utils.StringUtils;
+import io.ballerina.runtime.api.utils.XmlUtils;
+import io.ballerina.runtime.api.values.BArray;
 import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.api.values.BString;
 import io.ballerina.runtime.observability.ObservabilityConstants;
 import io.ballerina.runtime.observability.ObserveUtils;
-import io.ballerina.runtime.scheduling.Strand;
-import io.ballerina.runtime.types.BAnnotatableType;
-import io.ballerina.runtime.types.BArrayType;
-import io.ballerina.runtime.types.BStructureType;
-import io.ballerina.runtime.util.exceptions.BallerinaConnectorException;
-import io.ballerina.runtime.values.HandleValue;
 import org.ballerinalang.messaging.rabbitmq.observability.RabbitMQMetricsUtil;
 import org.ballerinalang.messaging.rabbitmq.observability.RabbitMQObservabilityConstants;
 import org.ballerinalang.messaging.rabbitmq.observability.RabbitMQObserverContext;
@@ -96,10 +92,11 @@ public class MessageDispatcher {
     }
 
     private String getQueueNameFromConfig(BObject service) {
-        BMap serviceConfig = (BMap) ((BAnnotatableType) service.getType())
-                .getAnnotation(RabbitMQConstants.PACKAGE_RABBITMQ_FQN, RabbitMQConstants.SERVICE_CONFIG);
+        BMap serviceConfig = (BMap) ((AnnotatableType) service.getType())
+                .getAnnotation(StringUtils.fromString(RabbitMQConstants.PACKAGE_RABBITMQ_FQN + ":"
+                               + RabbitMQConstants.SERVICE_CONFIG));
         @SuppressWarnings(RabbitMQConstants.UNCHECKED)
-        BMap<Strand, Object> queueConfig =
+        BMap<BString, Object> queueConfig =
                 (BMap) serviceConfig.getMapValue(RabbitMQConstants.ALIAS_QUEUE_CONFIG);
         return queueConfig.getStringValue(RabbitMQConstants.QUEUE_NAME).getValue();
     }
@@ -156,15 +153,15 @@ public class MessageDispatcher {
         CountDownLatch countDownLatch = new CountDownLatch(1);
         try {
             Callback callback = new RabbitMQResourceCallback(countDownLatch, channel, queueName,
-                                                                         message.length);
+                                                             message.length);
             BObject messageBObject = getMessageBObject(message, deliveryTag, properties);
             executeResourceOnMessage(callback, messageBObject, true);
             countDownLatch.await();
         } catch (InterruptedException e) {
             RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
             Thread.currentThread().interrupt();
-            throw new RabbitMQConnectorException(RabbitMQConstants.THREAD_INTERRUPTED);
-        } catch (AlreadyClosedException | BallerinaConnectorException exception) {
+            throw RabbitMQUtils.returnErrorValue(RabbitMQConstants.THREAD_INTERRUPTED);
+        } catch (AlreadyClosedException | BError exception) {
             RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
             handleError(message, deliveryTag, properties);
         }
@@ -178,17 +175,17 @@ public class MessageDispatcher {
             BObject messageBObject = getMessageBObject(message, deliveryTag, properties);
             CountDownLatch countDownLatch = new CountDownLatch(1);
             Callback callback = new RabbitMQResourceCallback(countDownLatch, channel, queueName,
-                                                                         message.length);
+                                                             message.length);
             executeResourceOnMessage(callback, messageBObject,
                                      true, forContent, true);
             countDownLatch.await();
-        } catch (BallerinaConnectorException | UnsupportedEncodingException exception) {
+        } catch (BError | UnsupportedEncodingException exception) {
             RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
             handleError(message, deliveryTag, properties);
         } catch (InterruptedException e) {
             RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
             Thread.currentThread().interrupt();
-            throw new RabbitMQConnectorException(RabbitMQConstants.THREAD_INTERRUPTED);
+            throw RabbitMQUtils.returnErrorValue(RabbitMQConstants.THREAD_INTERRUPTED);
         }
     }
 
@@ -198,38 +195,38 @@ public class MessageDispatcher {
             case TypeTags.STRING_TAG:
                 return StringUtils.fromString(new String(message, StandardCharsets.UTF_8.name()));
             case TypeTags.JSON_TAG:
-                return JSONParser.parse(new String(message, StandardCharsets.UTF_8.name()));
+                return JsonUtils.parse(new String(message, StandardCharsets.UTF_8.name()));
             case TypeTags.XML_TAG:
-                return XMLFactory.parse(new String(message, StandardCharsets.UTF_8.name()));
+                return XmlUtils.parse(new String(message, StandardCharsets.UTF_8.name()));
             case TypeTags.FLOAT_TAG:
                 return Float.parseFloat(new String(message, StandardCharsets.UTF_8.name()));
             case TypeTags.INT_TAG:
                 return Integer.parseInt(new String(message, StandardCharsets.UTF_8.name()));
             case TypeTags.RECORD_TYPE_TAG:
-                return JSONUtils.convertJSONToRecord(JSONParser.parse(new String(message,
-                                                                                 StandardCharsets.UTF_8.name())),
-                                                     (BStructureType) dataType);
+                return JsonUtils.convertJSONToRecord(JsonUtils.parse(new String(message,
+                                                                                StandardCharsets.UTF_8.name())),
+                                                     (StructureType) dataType);
             case TypeTags.ARRAY_TAG:
-                if (((BArrayType) dataType).getElementType().getTag() == TypeTags.BYTE_TAG) {
+                if (((BArray) dataType).getElementType().getTag() == TypeTags.BYTE_TAG) {
                     return message;
                 } else {
                     RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
-                    throw new RabbitMQConnectorException("Only type byte[] is supported in data binding.");
+                    throw RabbitMQUtils.returnErrorValue("Only type byte[] is supported in data binding.");
                 }
             default:
                 RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
-                throw new RabbitMQConnectorException(
+                throw RabbitMQUtils.returnErrorValue(
                         "The content type of the message received does not match the resource signature type.");
         }
     }
 
     private BObject getMessageBObject(byte[] message, long deliveryTag, AMQP.BasicProperties properties) {
         BObject messageBObject = ValueCreator.createObjectValue(RabbitMQConstants.PACKAGE_ID_RABBITMQ,
-                                                                     RabbitMQConstants.MESSAGE_OBJECT);
+                                                                RabbitMQConstants.MESSAGE_OBJECT);
         RabbitMQTransactionContext transactionContext =
                 (RabbitMQTransactionContext) channelObj.getNativeData(RabbitMQConstants.RABBITMQ_TRANSACTION_CONTEXT);
         messageBObject.set(RabbitMQConstants.DELIVERY_TAG, deliveryTag);
-        messageBObject.set(RabbitMQConstants.JAVA_CLIENT_CHANNEL, new HandleValue(channel));
+        messageBObject.set(RabbitMQConstants.JAVA_CLIENT_CHANNEL, ValueCreator.createHandleValue(channel));
         messageBObject.set(RabbitMQConstants.MESSAGE_CONTENT, ValueCreator.createArrayValue(message));
         messageBObject.set(RabbitMQConstants.AUTO_ACK_STATUS, autoAck);
         messageBObject.set(RabbitMQConstants.MESSAGE_ACK_STATUS, false);
@@ -241,14 +238,14 @@ public class MessageDispatcher {
             String correlationId = properties.getCorrelationId();
             BMap<BString, Object> basicProperties =
                     ValueCreator.createRecordValue(RabbitMQConstants.PACKAGE_ID_RABBITMQ,
-                                                      RabbitMQConstants.RECORD_BASIC_PROPERTIES);
+                                                   RabbitMQConstants.RECORD_BASIC_PROPERTIES);
             Object[] values = new Object[4];
             values[0] = replyTo;
             values[1] = contentType;
             values[2] = contentEncoding;
             values[3] = correlationId;
             messageBObject.set(RabbitMQConstants.BASIC_PROPERTIES,
-                                   ValueCreator.createRecordValue(basicProperties, values));
+                               ValueCreator.createRecordValue(basicProperties, values));
         }
         return messageBObject;
     }
@@ -264,10 +261,10 @@ public class MessageDispatcher {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
-            throw new RabbitMQConnectorException(RabbitMQConstants.THREAD_INTERRUPTED);
-        } catch (AlreadyClosedException | BallerinaConnectorException exception) {
+            throw RabbitMQUtils.returnErrorValue(RabbitMQConstants.THREAD_INTERRUPTED);
+        } catch (AlreadyClosedException | BError exception) {
             RabbitMQMetricsUtil.reportError(channel, RabbitMQObservabilityConstants.ERROR_TYPE_CONSUME);
-            throw new RabbitMQConnectorException("Error occurred in RabbitMQ service. ");
+            throw RabbitMQUtils.returnErrorValue("Error occurred in RabbitMQ service. ");
         }
     }
 
--- rabbitmq-native/src/main/java/org/ballerinalang/messaging/rabbitmq/RabbitMQErrorResourceCallback.java
+++ rabbitmq-native/src/main/java/org/ballerinalang/messaging/rabbitmq/RabbitMQErrorResourceCallback.java
@@ -19,7 +19,6 @@
 package org.ballerinalang.messaging.rabbitmq;
 
 import io.ballerina.runtime.api.async.Callback;
-import io.ballerina.runtime.services.ErrorHandlerUtils;
 import org.ballerinalang.messaging.rabbitmq.observability.RabbitMQMetricsUtil;
 import org.ballerinalang.messaging.rabbitmq.observability.RabbitMQObservabilityConstants;
 
@@ -46,6 +45,6 @@ public class RabbitMQErrorResourceCallback implements Callback {
     public void notifyFailure(io.ballerina.runtime.api.values.BError error) {
         countDownLatch.countDown();
         RabbitMQMetricsUtil.reportError(RabbitMQObservabilityConstants.ERROR_TYPE_ERROR_DISPATCH);
-        ErrorHandlerUtils.printError("RabbitMQ Error: " + error.getPrintableStackTrace());
+        error.printStackTrace();
     }
 }
