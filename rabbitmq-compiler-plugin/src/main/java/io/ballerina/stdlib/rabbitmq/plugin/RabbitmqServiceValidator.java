2b2e16f1b0b954475102916b745167d36bf41200
“aashikam” <arshika.nm@gmail.com>
2021-04-08 23:00:21 +0530

    Add compiler plugin

---
+++ rabbitmq-compiler-plugin/src/main/java/io/ballerina/stdlib/rabbitmq/plugin/RabbitmqServiceValidator.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.rabbitmq.plugin;
+
+import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.symbols.AnnotationSymbol;
+import io.ballerina.compiler.api.symbols.MethodSymbol;
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
+import io.ballerina.compiler.api.symbols.ServiceAttachPoint;
+import io.ballerina.compiler.api.symbols.ServiceAttachPointKind;
+import io.ballerina.compiler.api.symbols.ServiceDeclarationSymbol;
+import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.syntax.tree.FunctionDefinitionNode;
+import io.ballerina.compiler.syntax.tree.Node;
+import io.ballerina.compiler.syntax.tree.NodeList;
+import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
+import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
+import io.ballerina.stdlib.rabbitmq.plugin.PluginConstants.CompilationErrors;
+import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * RabbitMQ service compilation validator.
+ */
+public class RabbitmqServiceValidator {
+    private final SyntaxNodeAnalysisContext context;
+    private final NodeList<Node> memberNodes;
+
+    public RabbitmqServiceValidator(SyntaxNodeAnalysisContext context) {
+        this.context = context;
+        ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) context.node();
+        this.memberNodes = serviceDeclarationNode.members();
+    }
+
+    public void validate() {
+        validateAnnotation(this.context);
+        FunctionDefinitionNode onMessage = null;
+        FunctionDefinitionNode onRequest = null;
+        FunctionDefinitionNode onError = null;
+
+        if (!memberNodes.isEmpty()) {
+            for (Node node : memberNodes) {
+                FunctionDefinitionNode functionDefinitionNode = (FunctionDefinitionNode) node;
+                if (node.kind() == SyntaxKind.OBJECT_METHOD_DEFINITION) {
+                    MethodSymbol methodSymbol = PluginUtils.getMethodSymbol(context, functionDefinitionNode);
+                    Optional<String> functionName = methodSymbol.getName();
+                    if (functionName.isPresent()) {
+                        if (functionName.get().equals(PluginConstants.ON_MESSAGE_FUNC)) {
+                            onMessage = functionDefinitionNode;
+                        } else if (functionName.get().equals(PluginConstants.ON_REQUEST_FUNC)) {
+                            onRequest = functionDefinitionNode;
+                        } else if (functionName.get().equals(PluginConstants.ON_ERROR_FUNC)) {
+                            onError = functionDefinitionNode;
+                        } else {
+                            validateNonRabbitmqFunction(functionDefinitionNode);
+                        }
+                    }
+                }
+            }
+        }
+        new RabbitmqFunctionValidator(context, onMessage, onRequest, onError).validate();
+    }
+
+    public void validateNonRabbitmqFunction(FunctionDefinitionNode functionDefinitionNode) {
+        if (PluginUtils.isRemoteFunction(context, functionDefinitionNode)) {
+            context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.INVALID_REMOTE_FUNCTION,
+                    DiagnosticSeverity.ERROR, functionDefinitionNode.location()));
+        }
+    }
+
+    private void validateAnnotation(SyntaxNodeAnalysisContext context) {
+        SemanticModel semanticModel = context.semanticModel();
+        ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) context.node();
+        Optional<Symbol> symbol = semanticModel.symbol(serviceDeclarationNode);
+        if (symbol.isPresent()) {
+            ServiceDeclarationSymbol serviceDeclarationSymbol = (ServiceDeclarationSymbol) symbol.get();
+            Optional<ServiceAttachPoint> attachPoint = serviceDeclarationSymbol.attachPoint();
+            if (attachPoint.isEmpty()) {
+                List<AnnotationSymbol> symbolList = serviceDeclarationSymbol.annotations();
+                if (symbolList.isEmpty()) {
+                    context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.NO_ANNOTATION,
+                            DiagnosticSeverity.ERROR, serviceDeclarationNode.location()));
+                } else if (symbolList.size() > 1) {
+                    context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.INVALID_ANNOTATION_NUMBER,
+                            DiagnosticSeverity.ERROR, serviceDeclarationNode.location()));
+                } else {
+                    AnnotationSymbol annotationSymbol = symbolList.get(0);
+                    Optional<ModuleSymbol> moduleSymbolOptional = annotationSymbol.getModule();
+                    if (moduleSymbolOptional.isEmpty()) {
+                        context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.INVALID_ANNOTATION,
+                                DiagnosticSeverity.ERROR, serviceDeclarationNode.location()));
+                    } else {
+                        ModuleSymbol moduleSymbol = moduleSymbolOptional.get();
+                        if (!moduleSymbol.id().orgName().equals(PluginConstants.PACKAGE_ORG) ||
+                                !moduleSymbol.id().moduleName().equals(PluginConstants.PACKAGE_PREFIX)) {
+                            context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.INVALID_ANNOTATION,
+                                    DiagnosticSeverity.ERROR, serviceDeclarationNode.location()));
+                        }
+                    }
+                }
+            } else {
+                if (attachPoint.get().kind() != ServiceAttachPointKind.STRING_LITERAL
+                        && serviceDeclarationSymbol.annotations().isEmpty()) {
+                    context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.INVALID_SERVICE_NAME,
+                            DiagnosticSeverity.ERROR, serviceDeclarationNode.location()));
+                }
+            }
+        }
+    }
+}
--- rabbitmq-compiler-plugin/src/main/java/module-info.java
+++ rabbitmq-compiler-plugin/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,11 +16,8 @@
  * under the License.
  */
 
-module io.ballerina.stdlib.rabbitmq.runtime {
-    requires com.rabbitmq.client;
-    requires io.ballerina.runtime;
-    requires org.slf4j;
-    requires java.transaction.xa;
-    exports org.ballerinalang.messaging.rabbitmq.util;
-    exports org.ballerinalang.messaging.rabbitmq;
+module io.ballerina.stdlib.rabbitmq.plugin {
+    requires io.ballerina.lang;
+    requires io.ballerina.parser;
+    requires io.ballerina.tools.api;
 }
